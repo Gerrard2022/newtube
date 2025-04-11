6100c0afac90fbf80e6f78041a59c1192694c88c
Gerrard2022 <kaberukagerard8@gmail.com>
2025-04-12 00:07:50 +0200

    adds webhooks for clerk

---
+++ drizzle/0000_complete_wolverine.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "Users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"clerk_id" text NOT NULL,
+	"name" text NOT NULL,
+	"image_url" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "Users_clerk_id_unique" UNIQUE("clerk_id")
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX "clerk_id_index" ON "Users" USING btree ("clerk_id");
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,8 @@
 import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 
-export const users = pgTable("Users", {
+export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
-    clerkId: text("name").unique().notNull(),
+    clerkId: text("clerk_id").unique().notNull(),
     name: text("name").notNull(),
     //TODO: Add banner fields
     imageUrl: text("image_url").notNull(),
