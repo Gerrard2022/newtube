0ae0f2eac86b6259ea12fbe1c432200f6a9f497c
Gerrard2022 <kaberukagerard8@gmail.com>
2025-05-05 12:32:04 +0200

    starting mux integrations

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -2,10 +2,19 @@ import { z } from 'zod'
 import { db } from "@/db";
 import { videos } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { mux } from '@/lib/mux';
 
 export const videosRouter = createTRPCRouter({
     create: protectedProcedure.mutation(async ({ ctx }) => {
         const { id: userId } = ctx.user;
+
+        const upload = await mux.video.uploads.create({
+            new_asset_settings: {
+                passthrough: userId,
+                playback_policy: ["public"],
+            },
+            cors_origin: "*" //TODO: in production set ur URL
+        })
 
         const [video] = await db
             .insert(videos)
@@ -17,6 +26,7 @@ export const videosRouter = createTRPCRouter({
 
         return {
             video: video,
+            url: upload.url,
         };
     }),
 });
